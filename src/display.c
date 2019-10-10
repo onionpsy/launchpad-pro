dab1b748242248814f17e33c898a81166258172c
onionpsy <onionpsy@protonmail.com>
2019-10-10 12:53:10 +0200

    use display_plot_led

--- src/display.c
+++ src/display.c
@@ -13,13 +13,13 @@ void display_plot_led(u8 index, u16 color) {
 
 void display_fill_all(u16 color) {
     for (u8 i = 0; i < 99; ++i) {
-        display_draw_pad(i, color);
+        display_plot_led(i, color);
     }    
 }
 void display_fill_pads(u16 color) {
     for (u8 i = 1; i < 9; ++i) {
         for (u8 j = 1; j < 9; ++j) {
-            display_draw_pad(j * 10 + i, color);
+            display_plot_led(j * 10 + i, color);
         }
     }
 };
@@ -29,12 +29,12 @@ void display_draw_faders(u8 index, u8 *values, u16 color, u16 background_color) 
     for (u8 i = 1; i < 9; ++i) {
         for (u8 j = 2; j < 9; ++j) {
             if (j <= values[i]) {
-                display_draw_pad(
+                display_plot_led(
                     j * 10 + i,
                     color
                 );
             } else {
-                display_draw_pad(
+                display_plot_led(
                     j * 10 + i,
                     background_color
                 );
@@ -46,13 +46,12 @@ void display_draw_faders(u8 index, u8 *values, u16 color, u16 background_color) 
 u8 display_change_page(u8 index, FaderPage *pages) {
     // Clear pages buttons
     for (u8 i = 1; i < 9; ++i) {
-        display_draw_
-        hal_plot_led(TYPEPAD, i * 10 + 9, 0, 0, 10);
+        display_plot_led(i * 10 + 9, 0x000);
     }
 
     for (u8 i = 0; i < sizeof(pages->values) / sizeof(pages[0]); ++i) {
         if (pages[i].index == index) {
-            hal_plot_led(TYPEPAD, index, MAXLED, MAXLED, MAXLED);
+            display_plot_led(index, MAXLED);
             return i;
         }
     }
@@ -62,7 +61,7 @@ u8 display_change_page(u8 index, FaderPage *pages) {
 
 void display_draw_mutes(u8 index, u8 *mutes, u16 muted_color, u16 unmuted_color) {
     for (u8 i = 1; i < 9; ++i) {
-        display_draw_pad(
+        display_plot_led(
             i + 10,
             mutes[i]? muted_color : unmuted_color
         );
@@ -72,17 +71,17 @@ void display_draw_mutes(u8 index, u8 *mutes, u16 muted_color, u16 unmuted_color)
 void display_draw_piano(u8 index) {
     const u16 black_key = 0x111;
     const u16 white_key = 0x222;
-    display_draw_pad(82, black_key);
-    display_draw_pad(83, black_key);
-    display_draw_pad(85, black_key);
-    display_draw_pad(86, black_key);
-    display_draw_pad(87, black_key);
-    display_draw_pad(71, white_key);
-    display_draw_pad(72, white_key);
-    display_draw_pad(73, white_key);
-    display_draw_pad(74, white_key);
-    display_draw_pad(75, white_key);
-    display_draw_pad(76, white_key);
-    display_draw_pad(77, white_key);
-    display_draw_pad(78, white_key);
+    display_plot_led(82, black_key);
+    display_plot_led(83, black_key);
+    display_plot_led(85, black_key);
+    display_plot_led(86, black_key);
+    display_plot_led(87, black_key);
+    display_plot_led(71, white_key);
+    display_plot_led(72, white_key);
+    display_plot_led(73, white_key);
+    display_plot_led(74, white_key);
+    display_plot_led(75, white_key);
+    display_plot_led(76, white_key);
+    display_plot_led(77, white_key);
+    display_plot_led(78, white_key);
 }
