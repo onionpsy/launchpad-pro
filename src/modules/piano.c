1f62af150d8516520847a1459ec642c67bde7452
onionpsy <onionpsy@protonmail.com>
2019-10-24 20:53:48 +0200

    fox piano notes

--- src/modules/piano.c
+++ src/modules/piano.c
@@ -1,13 +1,13 @@
 #include "piano.h"
 
 void piano_handler(Piano *piano, u8 index, u8 value) {
-    u8 size = sizeof piano->pads_intervals / sizeof *piano->pads_intervals;
-    u8 midi_note = in_array(index - piano->c, piano->pads_intervals, size);
-
     // 17 = top right pad
     if (index < piano->c || index > piano->c + 17) {
         return;
     }
+
+    u8 size = sizeof piano->pads_intervals / sizeof *piano->pads_intervals;
+    u8 midi_note = in_array(index - piano->c, piano->pads_intervals, size);
 
     if (value > 0) {
         midi_send(DINMIDI, NOTEON, piano->midi_channel, midi_note + (12 * piano->octave), value);
